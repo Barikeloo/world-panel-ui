46d294af1520b0732bcb373e8344931f8035186d
BK ^^ <84545556+Barikeloo@users.noreply.github.com>
2022-04-11 23:09:49 +0200

    Delete world-panel directory

--- world-panel/Server/s-main.lua
+++
@@ -1,7 +0,0 @@
-RegisterServerEvent('world-panel:sendToDimension', function(world)
-    SetPlayerRoutingBucket(source, tonumber(world))
-end)
-
-RegisterServerEvent('world-panel:ComeBack', function()
-    SetPlayerRoutingBucket(source, 0)
-end)
